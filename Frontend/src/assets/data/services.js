79ca259f988a35e5d5e1f306d7fc39307f889b12
Shree271016 <pudasainishreekrishna1@gmail.com>
2025-04-08 09:24:50 +0545

    add assets file on git hub

---
+++ Frontend/src/assets/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+  {
+    name: "Cancer Care",
+    desc: "Comprehensive cancer treatment using advanced technologies and compassionate support to improve health and enhance patients' quality of life.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Labor & Delivery",
+    desc: "Dedicated care for mothers and newborns, ensuring safe pregnancies, smooth deliveries, and healthy beginnings for growing families.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "Heart & Vascular",
+    desc: "Innovative cardiac care focused on treating heart conditions, improving vascular health, and enhancing patients' overall well-being and longevity.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Mental Health",
+    desc: "Comprehensive mental health support offering therapy, counseling, and personalized care to promote emotional well-being and psychological resilience.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "Neurology",
+    desc: "Advanced neurological care for conditions affecting the brain, spine, and nerves, improving functionality and enhancing patients' quality of life.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Burn Treatment",
+    desc: "Expert burn care focusing on pain management, recovery, and rehabilitation to restore patients' health and confidence after injuries.",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+];
