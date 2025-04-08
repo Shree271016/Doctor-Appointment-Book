75e5c39552053ec5325e7972962471a2259f26d1
Shree271016 <pudasainishreekrishna1@gmail.com>
2025-04-08 09:13:08 +0545

    fixing issue for deploy on footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 
 
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
 import { RiLinkedinFill } from "react-icons/ri";
 import { AiFillYoutube, AiFillGithub, AiOutlineInstagram, } from "react-icons/ai"
 
