28ed8cc067e19de51a7877f1e9584fbbfb43de29
Shree271016 <pudasainishreekrishna1@gmail.com>
2025-04-10 13:49:12 +0545

    correct n file

--- Frontend/src/layout/Layout.jsx
+++ Frontend/src/layout/Layout.jsx
@@ -1,16 +1,21 @@
+// layout/Layout.js
 import { useState, useEffect } from 'react';
 import { AiOutlineArrowUp } from 'react-icons/ai';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import Routers from '../routes/Routers';
-import ScrollToTop from '../components/ScrollTop/ScrollToTop';
+import ScrollToTop from '../components/ScrollTop/ScrollToTop';  
 
 const Layout = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const toggleVisibility = () => {
-      setIsVisible(window.pageYOffset > 300);
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
     };
 
     window.addEventListener('scroll', toggleVisibility);
@@ -26,9 +31,9 @@ const Layout = () => {
 
   return (
     <>
-      <ScrollToTop /> 
       <Header />
       <main>
+        <ScrollToTop />
         <Routers />
       </main>
       <Footer />
