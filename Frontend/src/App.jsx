51a0cc7e4a83bb74348ef43ec280f92c940341da
Shree271016 <pudasainishreekrishna1@gmail.com>
2025-04-10 13:21:32 +0545

    remmoved the duplicate AuthContextProvider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css"
-import { AuthContextProvider } from "./context/AuthContext";
 import Layout from "./layout/Layout";
 function App() {
   return (
-    <AuthContextProvider>
-
+  
     <Layout />
-    </AuthContextProvider>
+
   )
 }
 
