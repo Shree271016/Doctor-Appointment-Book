d030fc1b89d9856ba569fa09eea9df102f989522
Shree271016 <pudasainishreekrishna1@gmail.com>
2025-04-10 17:03:32 +0545

    fixing issue on animation code change form app.css to tailwiind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,7 +4,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', //
+  safelist: [
+    'slide-in-text',
+    'continuous-scale-text',
+    'rotate-bounce',
+    'hover-animate-text',
+  ],
+  darkMode: 'class', 
   theme: {
     extend: {
       colors: {
@@ -19,7 +25,39 @@ export default {
       boxShadow:{
         planeShadow:"rgba(17,12,46,0.15) 0px 48px 100px 0px",
         },
+        keyframes: {
+          slideIn: {
+            '0%': { transform: 'translateX(-100%)' },
+            '100%': { transform: 'translateX(0)' },
+          },
+          continuousScaleUp: {
+            '0%, 100%': { transform: 'scale(1)', opacity: '1' },
+            '50%': { transform: 'scale(1.1)', opacity: '1' },
+          },
+          rotateAndBounce: {
+            '0%': { transform: 'rotate(0deg) scale(0.8)', opacity: '0' },
+            '40%': { transform: 'rotate(360deg) scale(1.2)', opacity: '1' },
+            '60%': { transform: 'rotate(360deg) scale(0.95)' },
+            '80%': { transform: 'rotate(360deg) scale(1.05)' },
+            '100%': { transform: 'rotate(360deg) scale(1)' },
+          },
+          hoverPulse: {
+            '0%, 100%': { transform: 'scale(1)' },
+            '50%': { transform: 'scale(1.1)' },
+          }
+        },
+        animation: {
+          'slide-in': 'slideIn 3s ease-out infinite',
+          'scale-up': 'continuousScaleUp 3s ease-in-out infinite',
+          'rotate-bounce': 'rotateAndBounce 3s ease-out infinite',
+          'hover-pulse': 'hoverPulse 0.6s ease-in-out',
+        }
+
+
+
     },
+
+
   },
   plugins: [],
 }
